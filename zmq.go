3a329bbd15c55f59b5f54e2aff788ff75e28887f
Anthonin Bonnefoy <anthonin.bonnefoy@gmail.com>
2013-08-07 16:06:26 +0200

    Added basic send / recv with tests and benchmarks

--- zmq.go
+++ zmq.go
@@ -5,6 +5,7 @@ package zerogo
 #include <zmq.h>
 #include <stdlib.h>
 #include <string.h>
+#include <errno.h>
 */
 import "C"
 
@@ -24,6 +25,13 @@ const (
 	PUSH   = SocketType(C.ZMQ_PUSH)
 )
 
+type SendFlag C.int
+
+const (
+	SNDMORE  = SendFlag(C.ZMQ_SNDMORE)
+	DONTWAIT = SendFlag(C.ZMQ_DONTWAIT)
+)
+
 func (z ZeromqError) Error() string {
 	return string(z)
 }
@@ -33,58 +41,97 @@ type Context struct {
 }
 
 type Socket struct {
-	c unsafe.Pointer
-}
-
-func getZmqError() error {
-	return ZeromqError(C.zmq_errno())
-}
-
-func checkReturnType(r C.int) error {
-	if r == -1 {
-		return getZmqError()
-	}
-	return nil
+	s unsafe.Pointer
 }
 
 func NewContext() (ctx *Context, err error) {
 	ctx = &Context{}
-	ctx.c = C.zmq_ctx_new()
-	if ctx.c == nil {
-		err = getZmqError()
-	}
-	return
+	ctx.c, err = C.zmq_ctx_new()
+	return ctx, err
 }
 
 func (ctx *Context) Destroy() error {
-	r := C.zmq_ctx_destroy(ctx.c)
-	return checkReturnType(r)
+	_, err := C.zmq_ctx_destroy(ctx.c)
+	return err
 }
 
-func (ctx *Context) NewSocket(socketType SocketType) (socket *Socket, err error) {
-	socket = &Socket{}
-	socket.c = C.zmq_socket(ctx.c, C.int(socketType))
-	if socket.c == nil {
-		err = getZmqError()
-	}
-	return
+func (ctx *Context) NewSocket(socketType SocketType) (*Socket, error) {
+	s, err := C.zmq_socket(ctx.c, C.int(socketType))
+	socket := &Socket{s}
+	return socket, err
 }
 
-func (soc *Socket) Close() (err error) {
-	r := C.zmq_close(soc.c)
-	return checkReturnType(r)
+func (soc *Socket) Close() error {
+	_, err := C.zmq_close(soc.s)
+	return err
 }
 
 func (soc *Socket) Bind(address string) error {
 	addr := C.CString(address)
 	defer C.free(unsafe.Pointer(addr))
-	r := C.zmq_bind(soc.c, addr)
-	return checkReturnType(r)
+	r, err := C.zmq_bind(soc.s, addr)
+	if r == 0 {
+		return nil
+	}
+	return err
 }
 
 func (soc *Socket) Unbind(address string) error {
 	addr := C.CString(address)
 	defer C.free(unsafe.Pointer(addr))
-	r := C.zmq_bind(soc.c, addr)
-	return checkReturnType(r)
+	r, err := C.zmq_unbind(soc.s, addr)
+	if r == 0 {
+		return nil
+	}
+	return err
+}
+
+func (soc *Socket) Connect(address string) error {
+	addr := C.CString(address)
+	defer C.free(unsafe.Pointer(addr))
+	r, err := C.zmq_connect(soc.s, addr)
+	if r == 0 {
+		return nil
+	}
+	return err
+}
+
+func (soc *Socket) Disconnect(address string) error {
+	addr := C.CString(address)
+	defer C.free(unsafe.Pointer(addr))
+	r, err := C.zmq_disconnect(soc.s, addr)
+	if r == 0 {
+		return nil
+	}
+	return err
+}
+
+func (soc *Socket) Send(data []byte, flag SendFlag) error {
+	pdata := unsafe.Pointer(&data[0])
+	var msg C.zmq_msg_t;
+	sizeData := C.size_t(len(data))
+	C.zmq_msg_init_data(&msg, pdata, sizeData, nil, nil)
+	r, err := C.zmq_msg_send(&msg, soc.s, C.int(0))
+	C.zmq_msg_close(&msg)
+	if r == -1 {
+		return err
+	}
+	return nil
+}
+
+func (soc *Socket) Recv(flag SendFlag) ([]byte, error) {
+	var msg C.zmq_msg_t;
+	rc, err := C.zmq_msg_init(&msg);
+	defer C.zmq_msg_close (&msg);
+	if rc != 0 {
+		return []byte{}, err
+	}
+	rc, err = C.zmq_msg_recv(&msg, soc.s, 0);
+	if rc == -1 {
+		return []byte{}, err
+	}
+
+	sizeMsg := C.int(C.zmq_msg_size(&msg))
+	pdata := C.zmq_msg_data(&msg)
+	return C.GoBytes(pdata, sizeMsg), nil
 }
--- zmq_test.go
+++ zmq_test.go
@@ -5,7 +5,7 @@ import (
 	"testing"
 )
 
-func Test(t *testing.T) { check.TestingT(t) }
+func TestCheck(t *testing.T) { check.TestingT(t) }
 
 var _ = check.Suite(&ContextSuite{})
 var _ = check.Suite(&SocketSuite{})
@@ -14,36 +14,104 @@ type ContextSuite struct{}
 
 func (s *ContextSuite) TestContextCreateAndDestroy(c *check.C) {
 	ctx, err := NewContext()
-	c.Assert(err, check.IsNil, check.Commentf("Expected non nil context", err))
+	c.Assert(err, check.IsNil, check.Commentf("Expected non nil context"))
 	err = ctx.Destroy()
-	c.Assert(err, check.IsNil, check.Commentf("Error in context destruction", err))
+	c.Assert(err, check.IsNil, check.Commentf("Error in context destruction"))
 }
 
 type SocketSuite struct {
 	*Context
 }
 
-func (s *SocketSuite) SetUpTest(c *check.C) {
+func (s *SocketSuite) SetUpSuite(c *check.C) {
 	ctx, err := NewContext()
-	c.Assert(err, check.IsNil, check.Commentf("Error on context creation, expected nil error", err))
+	c.Assert(err, check.IsNil, check.Commentf("Error on context creation, expected nil error"))
 	s.Context = ctx
 }
 
-func (s *SocketSuite) TearDownTest(c *check.C) {
+func (s *SocketSuite) TearDownSuite(c *check.C) {
 	err := s.Destroy()
-	c.Assert(err, check.IsNil, check.Commentf("Error on context destruction", err))
+	c.Assert(err, check.IsNil, check.Commentf("Error on context destruction"))
 }
 
 func (ctx *SocketSuite) TestSocketCreate(c *check.C) {
 	socket, err := ctx.NewSocket(REQ)
-	c.Assert(err, check.IsNil, check.Commentf("Error on socket creation", err))
+	c.Assert(err, check.IsNil, check.Commentf("Error on socket creation"))
 	err = socket.Close()
-	c.Assert(err, check.IsNil, check.Commentf("Error on socket closing", err))
+	c.Assert(err, check.IsNil, check.Commentf("Error on socket closing"))
 }
 
-func (ctx *SocketSuite) TestSocketBind(c *check.C) {
-	socket, err := ctx.NewSocket(REQ)
-	c.Assert(err, check.IsNil, check.Commentf("Error on socket creation", err))
-	err = socket.Close()
-	c.Assert(err, check.IsNil, check.Commentf("Error on socket closing", err))
+func (ctx *SocketSuite) TestSocketTcpBind(c *check.C) {
+	endpoint := "tcp://127.0.0.1:5555"
+
+	socket, err := ctx.NewSocket(ROUTER)
+	c.Assert(err, check.IsNil, check.Commentf("Error on socket creation"))
+	defer socket.Close()
+
+	err = socket.Bind(endpoint)
+	c.Assert(err, check.IsNil, check.Commentf("Error on socket bind"))
+	err = socket.Unbind(endpoint)
+	c.Assert(err, check.IsNil, check.Commentf("Error on socket unbind"))
+}
+
+func (ctx *SocketSuite) TestSocketSend(c *check.C) {
+	endpoint := "tcp://127.0.0.1:9999"
+	data := []byte("test")
+
+	rep_socket, err := ctx.NewSocket(REP)
+	c.Assert(err, check.IsNil, check.Commentf("Error on socket creation"))
+	defer rep_socket.Close()
+
+	req_socket, err := ctx.NewSocket(REQ)
+	c.Assert(err, check.IsNil, check.Commentf("Error on socket creation"))
+	defer req_socket.Close()
+
+	err = rep_socket.Bind(endpoint)
+	c.Assert(err, check.IsNil, check.Commentf("Error on socket bind"))
+
+	err = req_socket.Connect(endpoint)
+	c.Assert(err, check.IsNil, check.Commentf("Error on socket bind"))
+
+	err = req_socket.Send(data, 0)
+	c.Assert(err, check.IsNil, check.Commentf("Error on send"))
+	response, err := rep_socket.Recv(0)
+
+	c.Assert(err, check.IsNil, check.Commentf("Error on receive"))
+	c.Assert(response, check.DeepEquals, data)
+	err = rep_socket.Send(response, 0)
+	c.Assert(err, check.IsNil, check.Commentf("Error on response send"))
+	response, err = req_socket.Recv(0)
+	c.Assert(err, check.IsNil, check.Commentf("Error on response receive"))
+	c.Assert(response, check.DeepEquals, data)
+
+	err = req_socket.Disconnect(endpoint)
+	c.Assert(err, check.IsNil, check.Commentf("Error on socket disconnect"))
+	err = rep_socket.Unbind(endpoint)
+	c.Assert(err, check.IsNil, check.Commentf("Error on socket unbind"))
+}
+
+
+func (ctx *SocketSuite) BenchmarkSocket(c *check.C) {
+	endpoint := "tcp://127.0.0.1:9999"
+	data := []byte("test")
+
+	rep_socket, _ := ctx.NewSocket(PUSH)
+	req_socket, _ := ctx.NewSocket(PULL)
+
+	rep_socket.Bind(endpoint)
+
+	req_socket.Connect(endpoint)
+
+	for i := 0; i < c.N; i++{
+		err := req_socket.Send(data, 0)
+		if err != nil { continue }
+		_, err = rep_socket.Recv(0)
+		if err != nil { continue }
+	}
+
+	req_socket.Disconnect(endpoint)
+	rep_socket.Unbind(endpoint)
+
+	rep_socket.Close()
+	req_socket.Close()
 }
