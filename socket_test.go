6b8f218f911d658f96ae5619d7691604aa824956
Anthonin Bonnefoy <anthonin.bonnefoy@gmail.com>
2013-08-08 18:44:34 +0200

    Added bench on multipart functions

--- socket_test.go
+++ socket_test.go
@@ -100,7 +100,7 @@ func TestSocketSend(t *testing.T) {
 	}
 	// Server receive request
 	response, err := env.server.Recv(0)
-	defer response.CloseMsg()
+	defer response.Close()
 	if err != nil {
 		t.Fatal("Error on server request receive", err)
 	}
@@ -114,7 +114,7 @@ func TestSocketSend(t *testing.T) {
 	}
 	// client receive response
 	response, err = env.client.Recv(0)
-	defer response.CloseMsg()
+	defer response.Close()
 	if err != nil {
 		t.Fatal("Error on client response receive", err)
 	}
@@ -137,13 +137,13 @@ func TestMultipart(t *testing.T) {
 	if err != nil {
 		t.Fatal("Error on multipart receive", err)
 	}
-	defer rep.CloseMsgs()
+	defer rep.Close()
 	if !reflect.DeepEqual(rep.Data, data) {
 		t.Fatalf("Multipart Receive %q, expected %q", rep.Data, data)
 	}
 }
 
-func benchmarkSendReceive(b *testing.B, sizeData int, endpoint string) {
+func benchamrkSimplePart(b *testing.B, sizeData int, endpoint string) {
 	env := &Env{Tester: b, serverType: PULL, endpoint: endpoint, clientType: PUSH}
 	env.setupEnv()
 	defer env.destroyEnv()
@@ -161,7 +161,7 @@ func benchmarkSendReceive(b *testing.B, sizeData int, endpoint string) {
 		if err != nil {
 			b.Fatal(err)
 		}
-		err = rep.CloseMsg()
+		err = rep.Close()
 		if err != nil {
 			b.Fatal(err)
 		}
@@ -170,25 +170,78 @@ func benchmarkSendReceive(b *testing.B, sizeData int, endpoint string) {
 }
 
 func Benchmark1BSendReceiveTcp(b *testing.B) {
-	benchmarkSendReceive(b, 1, TCP_ENDPOINT)
+	benchamrkSimplePart(b, 1, TCP_ENDPOINT)
 }
 
 func Benchmark1KBSendReceiveTcp(b *testing.B) {
-	benchmarkSendReceive(b, 1e3, TCP_ENDPOINT)
+	benchamrkSimplePart(b, 1e3, TCP_ENDPOINT)
 }
 
 func Benchmark1MBSendReceiveTcp(b *testing.B) {
-	benchmarkSendReceive(b, 1e6, TCP_ENDPOINT)
+	benchamrkSimplePart(b, 1e6, TCP_ENDPOINT)
 }
 
 func Benchmark1BSendReceiveInproc(b *testing.B) {
-	benchmarkSendReceive(b, 1, INPROC_ENDPOINT+"_1b")
+	benchamrkSimplePart(b, 1, INPROC_ENDPOINT+"_1b")
 }
 
 func Benchmark1KBSendReceiveInproc(b *testing.B) {
-	benchmarkSendReceive(b, 1e3, INPROC_ENDPOINT+"_1K")
+	benchamrkSimplePart(b, 1e3, INPROC_ENDPOINT+"_1K")
 }
 
 func Benchmark1MBSendReceiveInproc(b *testing.B) {
-	benchmarkSendReceive(b, 1e6, INPROC_ENDPOINT+"_1M")
+	benchamrkSimplePart(b, 1e6, INPROC_ENDPOINT+"_1M")
+}
+
+func benchmarkMultipart(b *testing.B, numParts int, sizeData int, endpoint string) {
+	env := &Env{Tester: b, serverType: PULL, endpoint: endpoint, clientType: PUSH}
+	env.setupEnv()
+	defer env.destroyEnv()
+
+	data := make([][]byte, numParts)
+	for i := 0; i < numParts; i++ {
+		data[i] = make([]byte, sizeData)
+	}
+
+	b.ResetTimer()
+	var rep *MessageMultipart
+	for i := 0; i < b.N; i++ {
+		err := env.client.SendMultipart(data, 0)
+		if err != nil {
+			b.Fatal(err)
+		}
+		rep, err = env.server.RecvMultipart(0)
+		if err != nil {
+			b.Fatal(err)
+		}
+		err = rep.Close()
+		if err != nil {
+			b.Fatal(err)
+		}
+	}
+	b.StopTimer()
+}
+
+func Benchmark10BMultipartTcp(b *testing.B) {
+	benchmarkMultipart(b, 10, 1, TCP_ENDPOINT)
+}
+
+func Benchmark10KBMultipartTcp(b *testing.B) {
+	benchmarkMultipart(b, 10, 1e3, TCP_ENDPOINT)
+}
+
+func Benchmark10MBMultipartTcp(b *testing.B) {
+	benchmarkMultipart(b, 10, 1e6, TCP_ENDPOINT)
+}
+
+func Benchmark10BMultipartInproc(b *testing.B) {
+	benchmarkMultipart(b, 10, 1, INPROC_ENDPOINT+"_1b")
+}
+
+func Benchmark10KBMultipartInproc(b *testing.B) {
+	benchmarkMultipart(b, 10, 1e3, INPROC_ENDPOINT+"_1K")
+}
+
+func Benchmark10MBMultipartInproc(b *testing.B) {
+	benchmarkMultipart(b, 10, 1e6, INPROC_ENDPOINT+"_1M")
 }
