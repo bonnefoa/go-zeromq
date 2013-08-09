c762f235145082437ff152c9234bd24541bec451
Anthonin Bonnefoy <anthonin.bonnefoy@gmail.com>
2013-08-09 10:45:05 +0200

    Added poll benchmark

--- poller_test.go
+++ poller_test.go
@@ -16,10 +16,30 @@ func TestPollSocket(t *testing.T) {
 		t.Fatal("Error on send", err)
 	}
 
-	item := PollItem{env.server, []PollEvent{POLLIN}}
-	items := &PollItems{ item  }
+	item := &PollItem{Socket:env.server, Events:POLLIN}
+	items := PollItems{ item  }
 	rc, err := items.Poll(-1 * time.Millisecond)
 	if rc != 1 {
 		t.Fatalf("Expected pollin to return 1, was %d, err is %q", rc, err)
+	}
+	if item.REvents != POLLIN {
+		t.Fatalf("Expected poll item to be filled with POLLIN, got %+v", item)
+	}
+}
+
+func BenchmarkPollSocket(b *testing.B) {
+	env := &Env{Tester: b, serverType: PULL, endpoint: TCP_ENDPOINT, clientType: PUSH}
+	env.setupEnv()
+	defer env.destroyEnv()
+
+	data := make([]byte, 1e3)
+	item := &PollItem{Socket:env.server, Events:POLLIN}
+	items := PollItems{ item  }
+	var resp *MessagePart
+	for i :=0; i < b.N; i++ {
+		env.client.Send(data, 0)
+		items.Poll(-1 * time.Millisecond)
+		resp, _ = env.server.Recv(0)
+		_ = resp.Close()
 	}
 }
